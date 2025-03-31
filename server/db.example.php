2ae17eddcecffa1dcc75e82c22a82b1b1820e575
John Horton <johnharoldhorton@gmail.com>
2025-03-31 13:37:11 -0500

    Update PDO attributes

--- server/db.example.php
+++ server/db.example.php
@@ -6,6 +6,7 @@ $database="{DBNAME}";
 
 $db = new PDO('mysql:host={DB HOST URL};dbname=' . $database .';charset=utf8', $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES , false);
+$db->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
 
 ?>
